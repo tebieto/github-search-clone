18b8c80e31802173aedfbda547ea41498d5a99bf
Terry Ebieto <tebieto@gmail.com>
2021-07-11 09:17:35 +0100

    Added BrowserRouter and ApolloProvider

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {
+  ApolloClient,
+  createHttpLink,
+  InMemoryCache,
+  ApolloProvider,
+} from '@apollo/client';
+
+const link = createHttpLink({
+  uri: '/graphql',
+  credentials: 'same-origin',
+});
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link,
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <ApolloProvider client={client}>
+        <App />
+      </ApolloProvider>
+    </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
