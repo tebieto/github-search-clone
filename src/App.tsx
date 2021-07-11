1d3ad9f0b963ecc74a6de77723425eaf40a55f14
Terry Ebieto <tebieto@gmail.com>
2021-07-11 09:24:08 +0100

    Added Login Route, Added Loader and ErrorBoundary

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
+import { Route, Switch } from 'react-router';
 import './App.css';
-import Login from './pages/Login';
+import ErrorBoundary from './components/ErrorBoundary';
+import Loader from './components/Loader';
+const Login = lazy(() => import('./pages/Login'));
 
 function App(): JSX.Element {
-  return <Login />;
+  return (
+    <div>
+      <Switch>
+        <ErrorBoundary>
+          <Suspense fallback={<Loader />}>
+            <Route path={'/'} exact component={Login} />
+          </Suspense>
+        </ErrorBoundary>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
