714f37146fcda0bd006d2b1191c116092abe2db4
Terry Ebieto <tebieto@gmail.com>
2021-07-11 09:04:45 +0100

    implementing Login Page, setup prettier

---
+++ src/pages/Login/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { LoginContainer } from './styles';
+
+const Login = (): JSX.Element => {
+  return (
+    <LoginContainer>
+      <button>Hello</button>
+    </LoginContainer>
+  );
+};
+
+export default Login;
